483c291612adc03d4fdba9e9c0347d568c23205c
davisimoncelo <82762226+davisimoncelo@users.noreply.github.com>
2021-12-25 10:08:29 -0300

    Add files via upload

---
+++ contador.js
@@ -0,0 +1,22 @@
+Var x = 0;
+
+while(x < 10){
+    console.log(x);
+    x++;
+}
+
+for(x = 0;x < 10;x+++) { 
+    console.log(x); 
+}
+
+for (;;){
+    //comando...
+
+    minutos = ler tempo();
+    if(minutos > 60) {
+        break; // <<--parada hipotetica por tempo...
+    }
+}
+
+Math.log (valor inderteminado, definido pelo usuario);
+
